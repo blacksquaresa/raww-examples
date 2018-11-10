febb5af9f1e8c98b15da83c39eaf9d176ca6f828
garethc <gareth@blacksquare.co.za>
2018-11-10 14:24:31 +0200

    Update demo and add comments to the code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# raww-examples
+# Run as Web Worker Examples
 Examples of the use of the run-as-web-worker library
+
+
--- demo.ts
+++ demo.ts
@@ -1,6 +1,7 @@
 "use strict";
 import { RunAsWebWorker } from "run-as-web-worker";
 
+// Create some global objects to use for dependencies
 const func1 = () => {
   console.log("Function 1");
 };
@@ -11,6 +12,7 @@ function func3() {
   console.log("Function 3");
 }
 
+// This object contains references to functions, two levels deep
 const myObj = {
   boo: true,
   name: "my name",
@@ -22,32 +24,40 @@ const myObj = {
   }
 };
 
+// This array references our deep object again - and all the functions again
+const deep: any[] = [ myObj, func2 ];
+
+// Create a class to perform our demo functions
+// this class has three simple methods: add to add two numbers, sub to subtract and error to throw an error
 class Adder {
   constructor() {
     RunAsWebWorker()(this, "sub", {});
   }
 
+  // Use the decorator to hoist this function. We're adding all our dependencies here
   @RunAsWebWorker(
     { myString: "myString", myNumber: 42, myBool: true },
-    { func1, func2, func3, myObj }
+    { func1, func2, func3, myObj },
+    { deep }
   )
   add(x: number, y: number) {
     return new Promise((resolve, reject) => {
       myObj.func();
-      myObj.utils.func1();
       myObj.utils.func2();
-      myObj.utils.func3();
+      deep[0].utils.func3();
       const result = x + y;
       resolve(result);
     });
   }
 
+  // This function does not use the decorator, but is hoisted manually in the constructor
   sub = (x: number, y: number, z: number) => {
     return new Promise((resolve, reject) => {
       resolve(Number(x) - Number(y) - z);
     });
   };
 
+  // This method demonstrates what happens when an error occurs in your hoisted function
   @RunAsWebWorker()
   error(message: string) {
     return new Promise((resolve, reject) => {
@@ -56,6 +66,7 @@ class Adder {
   }
 }
 
+// This code creates references to the DOM elements we'll use later 
 let num1: HTMLInputElement =
   (document.getElementById("num1") as HTMLInputElement) ||
   new HTMLInputElement();
@@ -68,8 +79,12 @@ let answer: HTMLElement =
   document.getElementById("answer") || new HTMLElement();
 let answer2: HTMLElement =
   document.getElementById("answer2") || new HTMLElement();
+
+// Now create a new instance of the Adder class, which we'll also use later
 const adder = new Adder();
 
+// Add an event listerner to the button click event. When the button clicks, we'll do everything...
+// specifically, we'll call each of the three class methods, then call one of the methods deep in our dependencies, just to check they're still there.
 equals.addEventListener(
   "click",
   () => {
@@ -86,6 +101,9 @@ equals.addEventListener(
     adder.error("testing an error").catch((reason: string) => {
       console.log(reason);
     });
+
+    // Ensure we can still call the objects in our main file
+    deep[0].utils.func1();
   },
   false
 );
